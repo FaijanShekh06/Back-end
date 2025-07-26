fe62d81c6f0774baf6d66f6bcd03fcb7ce27f1f0
FaijanShekh06 <zaduu0301@gmail.com>
2025-07-26 18:49:19 +0530

    Express Js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@
 // import { a, b, c } from './mymodule.js'
 // console.log(a, b, c)
 
-// import obj from './mymodule.js'
-// console.log(obj);
+import obj from './mymodule.js'
+console.log(obj);
 (function (exports, require, module, __filename, __dirname) {
   //Module code actually lives here
 })
 
-const a = require('./mymodule2.js')
-console.log(a);
+// const a = require('./mymodule2.js')
+// console.log(a);
 
 
